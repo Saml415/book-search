66e21e25882818fd66c5f348d9a09d64f7289bb8
Sam Lloyd <bobdoe784@gmail.com>
2021-08-19 16:00:23 -0700

    Add adduser mutation to server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,20 @@
 // const { AuthenticationError } = require("apollo-server-express");
-
-// const { signToken } = require("../utils/auth");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {},
 
-  Mutation: {},
+  Mutation: {
+    async addUser(parent, { username, email, password }) {
+      const user = await User.create({ username, email, password });
+      if (!user) {
+        throw new AuthenticationError("Something is Wrong!");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
